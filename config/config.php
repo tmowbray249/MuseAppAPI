772d289bd3c58cc4e05d588de04132763218542d
tmowbray249 <39931617+tmowbray249@users.noreply.github.com>
2022-03-08 20:13:22 +0000

    removed secret key

--- config/config.php
+++ config/config.php
@@ -2,9 +2,10 @@
 
 const DEV_MODE = true;  # todo set to false before demos or putting live
 const BASEPATH = '/museapp/MuseAppAPI/';
+
+
 const DATABASE_DIS= "db/dis.sqlite";  # todo update
 const DATABASE_USER = "db/user.sqlite"; # todo update
-const SECRET_KEY = '.E^Dk>n%%[Qrj{D#I]11*t^#vw~2b7Gy|%S*74g]F9?n2rfV#:Ze9`Izf{>&Pdb';  # todo may be redundant
 
 ini_set('display_errors', DEV_MODE);
 ini_set('display_startup_errors', DEV_MODE);
